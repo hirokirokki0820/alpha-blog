7e63c86ba5a3d4a6e4abcf7276fc720ba9cc7ce9
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-10 22:14:37 +0900

    add create and destroy user sessions(login, logout)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "ログインに成功しました。"
+      redirect_to user
+    else
+      flash.now[:alert] = "メールアドレス、またはパスワードに間違いがあります。"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました。"
+    redirect_to root_path, status: :see_other
   end
 
 end
