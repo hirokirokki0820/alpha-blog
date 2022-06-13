3fdee4ec06b56dd20b7d163e7a8fd784dbf7e8dd
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-13 16:54:49 +0900

    Add categories create action and functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "新規カテゴリーが作成されました。"
+      redirect_to @category
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def index
@@ -10,5 +20,11 @@ class CategoriesController < ApplicationController
 
   def show
 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
