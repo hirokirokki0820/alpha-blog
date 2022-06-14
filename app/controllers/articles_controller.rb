34426cd094a993d39b93b5b993a616849af2cd55
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-14 21:59:54 +0900

    Add category form (with collection_select helper)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     #@article = Article.new(params[:article]) と同じ処理
     @article = Article.new(article_params)
     @article.user = current_user
@@ -58,7 +59,7 @@ class ArticlesController < ApplicationController
     end
     # Strong Parameters
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
